db2ddd5fa17414d01c56d69e69fb78b1cc3f0b06
K. Eaglin <kceaglin@gmail.com>
2018-06-02 19:24:15 -0400

    fetches images; need to make grid remain square with images inside

--- js/script.js
+++ js/script.js
@@ -1,29 +1,3 @@
-// const gallery = document.querySelector('.gallery');
-// const overlay = document.querySelector('.overlay');
-// const overlayImage = overlay.querySelector('img');
-// const overlayClose = overlay.querySelector('.close');
-
-// function handleClick(e) {
-//   const src = e.currentTarget.querySelector('img').src;
-//   overlayImage.src = src;
-//   overlay.classList.add('open');
-// }
-
-// function close() {
-//   overlay.classList.remove('open');
-// }
-
-// const digits = Array.from({ length: 50 }, () => [randomNumber(4), randomNumber(4)]).concat([[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]])
-
-// const html = digits.map(generateHTML).join('');
-// gallery.innerHTML = html;
-
-// const items = document.querySelectorAll('.item');
-
-// items.forEach(item => item.addEventListener('click', handleClick));
-
-// overlayClose.addEventListener('click', close);
-
 // get photos
 // need a function that will call the unsplash API to get a list of photos
 // save the list to a const array
@@ -59,7 +33,6 @@ function addImage() {
         const img = document.createElement("img");
         // img.createAttribute('src')
         img.setAttribute("src", res.url);
-        img.style = "object-fit: cover; overflow:hidden; width: 100%; height: 100%;";
         item.appendChild(img);
       })
       .catch(err => console.log("Something went wrong", err));
@@ -85,7 +58,7 @@ function addHoverListener() {
 
 function init() {
   addHoverListener();
-  addImage("rug");
+  // addImage("rug");
 }
 
 init();
