c8f20c1a5f7044ac8942ddebc9e99687c2190d25
K. Eaglin <kceaglin@gmail.com>
2018-06-02 17:56:49 -0400

    adding images

--- js/script.js
+++ js/script.js
@@ -34,20 +34,58 @@
 // fetch(`https://source.unsplash.com/weekly?${kw}`)
 // .then(res => addImage(res))
 // }
+
+// function imgSrc(kw) {
+//   fetch(`https://source.unsplash.com/weekly?${kw}`)
+//     .then(res => addImage(res))
+//     .catch(err => console.log("Something went wrong", err));
+// }
+
 // for each grid item, insert an image from the photos array
 // console.log(res.json())
 // img = document.createElement('img')
 // img.createAttribute('src', )
 // item.appendChild(photo)
+// function addImage(imgRes) {
+function addImage() {
+  // console.log("imgRes is", imgRes);
+  // console.log("imgRes.url", imgRes.url);
+  const items = document.querySelectorAll(".item");
+  items.forEach(item => {
+    // fetch(`https://source.unsplash.com/weekly?${kw}`)
+    fetch("https://source.unsplash.com/collection/155174")
+      .then(res => {
+        // console.log('testing')
+        const img = document.createElement("img");
+        // img.createAttribute('src')
+        img.setAttribute("src", res.url);
+        img.style = "object-fit: cover; overflow:hidden; width: 100%; height: 100%;";
+        item.appendChild(img);
+      })
+      .catch(err => console.log("Something went wrong", err));
+  });
+}
 
 // on every grid item, we need an event listener
 // listen for hover
 // on hover, show a mini product window
 // on click, go to product page
 
-function init() {
-  imgSrc(rug)
-  
+function showMini(e) {
+  e.target.style.background = "purple";
+  console.log("event listener working for", e.target);
 }
 
+function addHoverListener() {
+  const items = document.querySelectorAll("item");
+  items.forEach(item => {
+    item.addEventListener("mouseenter", showMini);
+  });
+}
 
+function init() {
+  addHoverListener();
+  addImage("rug");
+}
+
+init();
