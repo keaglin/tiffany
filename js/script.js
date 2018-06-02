b663a5b5ce5f04e7175641ef31bc69c498f5e2b1
K. Eaglin <kceaglin@gmail.com>
2018-06-02 16:21:39 -0400

    pseudocode logic for adding images and event listeners

--- js/script.js
+++ js/script.js
@@ -1,26 +1,53 @@
 // const gallery = document.querySelector('.gallery');
-const overlay = document.querySelector('.overlay');
-const overlayImage = overlay.querySelector('img');
-const overlayClose = overlay.querySelector('.close');
+// const overlay = document.querySelector('.overlay');
+// const overlayImage = overlay.querySelector('img');
+// const overlayClose = overlay.querySelector('.close');
 
-function handleClick(e) {
-  const src = e.currentTarget.querySelector('img').src;
-  overlayImage.src = src;
-  overlay.classList.add('open');
-}
+// function handleClick(e) {
+//   const src = e.currentTarget.querySelector('img').src;
+//   overlayImage.src = src;
+//   overlay.classList.add('open');
+// }
 
-
-function close() {
-  overlay.classList.remove('open');
-}
+// function close() {
+//   overlay.classList.remove('open');
+// }
 
 // const digits = Array.from({ length: 50 }, () => [randomNumber(4), randomNumber(4)]).concat([[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]])
 
 // const html = digits.map(generateHTML).join('');
 // gallery.innerHTML = html;
 
-const items = document.querySelectorAll('.item');
+// const items = document.querySelectorAll('.item');
 
-items.forEach(item => item.addEventListener('click', handleClick));
+// items.forEach(item => item.addEventListener('click', handleClick));
 
-overlayClose.addEventListener('click', close);
+// overlayClose.addEventListener('click', close);
+
+// get photos
+// need a function that will call the unsplash API to get a list of photos
+// save the list to a const array
+// return the const
+
+// display photos inside grid items
+// imgSrc = (kw) => {
+// fetch(`https://source.unsplash.com/weekly?${kw}`)
+// .then(res => addImage(res))
+// }
+// for each grid item, insert an image from the photos array
+// console.log(res.json())
+// img = document.createElement('img')
+// img.createAttribute('src', )
+// item.appendChild(photo)
+
+// on every grid item, we need an event listener
+// listen for hover
+// on hover, show a mini product window
+// on click, go to product page
+
+function init() {
+  imgSrc(rug)
+  
+}
+
+
