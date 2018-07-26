4e9d16954b40c7dcd597ed4dd6265b8878ded831
K. Eaglin <kceaglin@gmail.com>
2018-07-26 02:58:26 -0400

    dropdown looks need work but getting there; TODO search bar

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.dropdown-trigger');
-  var instances = M.Dropdown.init(elems);
+  var instances = M.Dropdown.init(elems, constrainWidth=false);
 });
 
 // get photos
